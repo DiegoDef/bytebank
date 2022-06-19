6f67d12c6e6d88247380cee35ba9af24deae8646
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2022-06-19 16:02:22 -0300

    requisicoes com api rest mockada

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,6 @@
+import { Observable } from 'rxjs';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { TransferenciaService } from "../services/transferencia.service";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -11,16 +13,22 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService) {};
+
   transferir() {
     console.log("Tranferência solicitada!");
 
     const valorEmitir = {valor: this.valor, destino: this.destino} as any;
-    this.aoTransferir.emit(valorEmitir);
-
-    this.limparCampo();
+    this.service.adicionar(valorEmitir).subscribe({
+      next: (resultado) => {
+      console.log(resultado);
+      this.limparCampos();
+      },
+      error: error => console.error(error)
+    });
   }
 
-  limparCampo() {
+  limparCampos() {
     this.valor = 0;
     this.destino = 0;
   }
--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Transferencia } from '../models/transferencia.models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
   private listaDeTransferencias: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.listaDeTransferencias = [];
   }
 
@@ -14,13 +18,14 @@ export class TransferenciaService {
     return this.listaDeTransferencias;
   }
 
-  adicionar(transferencia: any) {
-    this.hidratar(transferencia);
-    this.listaDeTransferencias.push(transferencia);
+  todas(): Observable<Transferencia[]> {
+    return this.httpClient.get<Transferencia[]>(this.url)
   }
 
-  private hidratar(transferencia) {
+  adicionar(transferencia: Transferencia): Observable<Transferencia[]> {
     transferencia = {...transferencia, data: new Date()}
+
+    return this.httpClient.post<Transferencia[]>(this.url, transferencia);
   }
 
 }
